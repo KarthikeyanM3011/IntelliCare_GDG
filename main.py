c6ea88eba52e06c64f5741f7253bfd8160caca91
KarthikeyanM3011 <133127501+KarthikeyanM3011@users.noreply.github.com>
2024-11-08 07:24:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from transformers import AutoTokenizer
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 from googletrans import Translator
-# from sentence_transformers import SentenceTransformer
 from langchain.prompts import PromptTemplate
 import google.generativeai as genai
 import PyPDF2
@@ -24,77 +23,6 @@ from googletrans import Translator
 from record import *
 from huggingface_hub import InferenceClient
 from store_web3 import *
-
-# def generate_response_chatbot(prompt):
-#     try:
-#         client = InferenceClient(api_key="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")
-
-#         messages = [
-#             {
-#                 "role": "user",
-#                 "content": prompt
-#             }
-#         ]
-
-#         stream = client.chat.completions.create(
-#             model="mistralai/Mixtral-8x7B-Instruct-v0.1", 
-#             messages=messages, 
-#             max_tokens=500,
-#             stream=True
-#         )
-
-#         res = ""
-#         for chunk in stream:
-#             res += chunk.choices[0].delta.content  
-#         return res
-#     except Exception as e:
-#         print(e)
-#         return "Something went wrong! Try again after sometime!"
-
-# def format_query_response_chatbot(user_query):
-#     prompt = f"""
-#     You are a highly trained medical assistant bot. It is extremely important that you provide **only accurate, well-founded, and medically reliable responses** to the user's queries.
-#     You should not make assumptions or provide vague, incomplete, or speculative answers. Your response must be based on well-established medical knowledge and principles.
-    
-#     If the query is related to medical conditions, treatments, symptoms, or any other health-related topics, you should respond with **careful, evidence-based, and precise information**. If you're unsure about any aspect of the response, it is better to indicate that medical advice should be sought from a healthcare provider. Always prioritize the safety and well-being of the user.
-
-#     If the user's query is **not related to any medical field**, respond with the message: 
-#     "Sorry! I am a medical bot, I cannot answer general questions."
-
-#     **Key instructions**:
-#     - Ensure your response is grounded in factual, medical evidence and knowledge.
-#     - Do **not** make up answers or give unverified information.
-#     - If the question concerns medical symptoms or conditions, be **cautious and detailed**, but avoid diagnosing or prescribing. Always advise users to consult a medical professional for confirmation.
-#     - If the query is about something non-medical, give a polite, clear, and firm redirection.
-    
-#     User Query: "{user_query}"
-#     """
-#     return prompt
-
-# chatbot_icon_html = """
-# <style>
-#     .chatbot-icon {
-#         position: fixed;
-#         bottom: 20px;
-#         right: 20px;
-#         width: 60px;
-#         height: 60px;
-#         border-radius: 50%;
-#         background-color: #4CAF50;
-#         color: white;
-#         display: flex;
-#         justify-content: center;
-#         align-items: center;
-#         font-size: 24px;
-#         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-#         cursor: pointer;
-#         z-index: 10;
-#     }
-# </style>
-# <div onclick="document.getElementById('chatbot-button').click()" class="chatbot-icon">💬</div>
-# """
-
-# st.markdown(chatbot_icon_html, unsafe_allow_html=True)
 
 selected=option_menu(None,['Home','MediScan','ReportChat', 'MedDB'],
     icons=['house','book','envelope','database'],
@@ -145,39 +73,6 @@ st.sidebar.markdown("# :orange[Karthikeyan M]")
 st.sidebar.markdown("# :orange[Arun Kumar R]")
 st.sidebar.markdown("# :orange[Logabaalan R S]")
 st.sidebar.markdown("# :orange[Barath Raj P]")
-
-# st.sidebar.markdown(
-#         """
-        
-#         Follow me on:
-
-#         LinkedIn → [Barath Raj P](https://www.linkedin.com/in/barathrajp/)
-
-#         """
-#     )
-
-
-# st.sidebar.markdown(
-#         """
-        
-#         Follow me on:
-
-#         LinkedIn → [Karthikeyan M](https://www.linkedin.com/in/karthikeyan-m30112004/)
-
-#         """
-#     )
-
-
-# st.sidebar.markdown(
-#         """
-        
-#         Follow me on:
-
-#         LinkedIn → [Arun Kumar R](https://www.linkedin.com/in/arun-kumar-99b841255/)
-
-#         """
-#     )
-
 languages = [
     'Select Your Language','English','Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Azerbaijani', 'Basque', 'Belarusian', 'Bengali', 'Bosnian',
     'Bulgarian', 'Catalan', 'Cebuano', 'Chichewa', 'Chinese (Simplified)', 'Chinese (Traditional)', 'Corsican', 'Croatian',
@@ -322,12 +217,6 @@ def home():
 
     if selected_language!='Select Your Language':
         selected_language_code = selected_language
-        # def show():
-        #     lottie_url = "https://lottie.host/00b6f013-63d8-4663-8829-edacfc48286d/v0PwepvHxL.json"
-        #     hello = load_lottieurl(lottie_url)
-        #     with st_lottie_spinner(hello,width=500, height=200,key="main"):
-        #         time.sleep(5)
-
         tokenizer = AutoTokenizer.from_pretrained("Karthikeyan-M3011/medflan-t5-large")
         model = AutoModelForSeq2SeqLM.from_pretrained("Karthikeyan-M3011/medflan-t5-large")
         def generate_response(query):
@@ -421,26 +310,11 @@ def home():
                                 placeholders.append((question_placeholder, response_placeholder))
                                 st.write("")
                         st.write("-----------------------------------------------------------------")
-                        # default_image='https://raw.githubusercontent.com/ArunKumar200510/MediChat-Anokha/main/images/empty.png'
-                        # img=find_image_url(med,data)
-                        # if img!=default_image:
-                        #     st.image(img, use_column_width=True)
-                        # else:
-                        #     st.image(img,width=10)
+
                         url=geturl(med)
                         if(url is not None and len(url)>0):
                             st.write(f" - {url[0].get('link')}")
                             url=None
-                
-            # selected_option = st.selectbox("Select an option if you want to Translate",languages )
-            # if(selected_option != 'Select an option if you want to Translate' ):
-            #     for idx, (question_placeholder, response_placeholder) in enumerate(placeholders):
-            #         translated_question = translate(ques[idx],selected_option )
-            #         translated_response = translate(res[idx], selected_option)
-            #         question_placeholder.markdown(f"**Question:** {translated_question}")
-            #         response_placeholder.markdown(f"**Answer:** {translated_response}")
-            #     selected_option = 'Select an option if you want to Translate'
-            # st.warning('As an AI language model, I must clarify that I am not a healthcare professional, so I cannot provide medical advice. However, I can offer some general information.', icon="⚠️")
 
 def extract_text_from_pdf(pdf_path): 
     pdf_bytes = BytesIO(pdf_path.read())
